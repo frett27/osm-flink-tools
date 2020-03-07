eb029145d8d0303b82efb645abc669ba1cfe81af
Patrice Freydiere <frett27@gmail.com>
2020-03-07 20:52:07 +0100

    move to gradle 5.2

--- src/main/java/org/frett27/spatialflink/inputs/process/ProcessOSMAvro.java
+++ src/main/java/org/frett27/spatialflink/inputs/process/ProcessOSMAvro.java
@@ -1,8 +1,16 @@
 package org.frett27.spatialflink.inputs.process;
 
+import java.io.File;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -21,87 +29,109 @@ import org.osm.avro.OSMType;
 
 public class ProcessOSMAvro {
 
-  public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-    ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-	if (args.length < 2)
-		throw new Exception("not enought parameters");
-	String inputPbf = args[0];
+		Options options = new Options();
 
-	System.out.println(" input pbf :" + inputPbf);
+		Option inputOption = new Option("in", "inputPbf", true, "Protocolbuffer input file");
+		inputOption.setRequired(true);
+		options.addOption(inputOption);
 
-	String outputResult = args[1];
-	System.out.println(" output result  :" + outputResult);
+		Option outputOption = new Option("out", "outputResultFolder", true, "output result folder");
+		outputOption.setRequired(true);
+		options.addOption(outputOption);
 
-    // String file = "C:/projets/OSMImport/rhone-alpes-latest.osm.pbf";
-    // String file = "F:/temp/france-latest.osm.pbf";
-    // String file = "C:/projets/OSMImport/rhone-alpes-latest.osm.pbf";
+		CommandLineParser parser = new DefaultParser();
+		CommandLine line = null;
+		try {
+			// parse the command line arguments
+			line = parser.parse(options, args);
+		} catch (ParseException exp) {
+			// oops, something went wrong
+			System.out.println(exp.getMessage());
+			// automatically generate the help statement
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("processosm", options);
+			System.exit(1);
+		}
 
-    OSMResultsStreams rs = ProcessOSM.constructOSMStreams(env, inputPbf);
+		if (!line.hasOption("in")) {
+			throw new Exception("must pass input");
+		}
 
-    DataSet<OSMEntity> f1 =
-        rs.retNodesWithAttributes.map(
-            new MapFunction<NodeEntity, OSMEntity>() {
-              public OSMEntity map(NodeEntity value) throws Exception {
-                ANode ret = new ANode();
-                ret.setFields(value.fields);
-                ret.setId(value.id);
-                ret.setX(value.x);
-                ret.setY(value.y);
+		String inputPbf = line.getOptionValue("in");
 
-                return new OSMEntity(OSMType.NODE, value.id, ret, null, null, null);
-              };
-            });
+		if (!line.hasOption("out")) {
+			throw new Exception("must pass output");
 
-    DataSet<OSMEntity> f2 =
-        rs.retPolygons.map(
-            new MapFunction<ComplexEntity, OSMEntity>() {
-              public OSMEntity map(ComplexEntity value) throws Exception {
+		}
 
-                AComplex c =
-                    new AComplex(value.id, ByteBuffer.wrap(value.shapeGeometry), value.fields);
+		String outputResult = line.getOptionValue("out");
 
-                return new OSMEntity(OSMType.POLYGON, value.id, null, null, c, null);
-              };
-            });
+		System.out.println(" input pbf :" + inputPbf);
 
-    DataSet<OSMEntity> f3 =
-        rs.retWaysEntities.map(
-            new MapFunction<ComplexEntity, OSMEntity>() {
-              @Override
-              public OSMEntity map(ComplexEntity value) throws Exception {
-                AComplex c =
-                    new AComplex(value.id, ByteBuffer.wrap(value.shapeGeometry), value.fields);
+		System.out.println(" output result  :" + outputResult);
 
-                return new OSMEntity(OSMType.WAY, value.id, null, c, null, null);
-              }
-            });
+		// String file = "C:/projets/OSMImport/rhone-alpes-latest.osm.pbf";
+		// String file = "F:/temp/france-latest.osm.pbf";
+		// String file = "C:/projets/OSMImport/rhone-alpes-latest.osm.pbf";
 
-    DataSet<OSMEntity> f4 =
-        rs.retRelations.map(
-            new MapFunction<Relation, OSMEntity>() {
-              public OSMEntity map(Relation value) {
+		OSMResultsStreams rs = ProcessOSM.constructOSMStreams(env, inputPbf);
 
-                ArrayList<ARelated> related = new ArrayList<ARelated>();
-                // grab all the relations
-                if (value.relatedObjects != null) {
-                	for (RelatedObject o : value.relatedObjects) {
-                		related.add(new ARelated(o.relatedId, o.type, o.role));
-                	}
-                }
+		DataSet<OSMEntity> f1 = rs.retNodesWithAttributes.map(new MapFunction<NodeEntity, OSMEntity>() {
+			public OSMEntity map(NodeEntity value) throws Exception {
+				ANode ret = new ANode();
+				ret.setFields(value.fields);
+				ret.setId(value.id);
+				ret.setX(value.x);
+				ret.setY(value.y);
 
-                ARelation r = new ARelation(value.id, value.fields, related);
+				return new OSMEntity(OSMType.NODE, value.id, ret, null, null, null);
+			};
+		});
 
-                return new OSMEntity(OSMType.RELATION, value.id, null, null, null, r);
-              };
-            });
-    
-    AvroOutputFormat<OSMEntity> avro = new AvroOutputFormat<OSMEntity>(OSMEntity.class);
-    
-    f1.union(f2).union(f3).union(f4).write(avro, outputResult);
-    
-    env.execute();
-    
-  }
+		DataSet<OSMEntity> f2 = rs.retPolygons.map(new MapFunction<ComplexEntity, OSMEntity>() {
+			public OSMEntity map(ComplexEntity value) throws Exception {
+
+				AComplex c = new AComplex(value.id, ByteBuffer.wrap(value.shapeGeometry), value.fields);
+
+				return new OSMEntity(OSMType.POLYGON, value.id, null, null, c, null);
+			};
+		});
+
+		DataSet<OSMEntity> f3 = rs.retWaysEntities.map(new MapFunction<ComplexEntity, OSMEntity>() {
+			@Override
+			public OSMEntity map(ComplexEntity value) throws Exception {
+				AComplex c = new AComplex(value.id, ByteBuffer.wrap(value.shapeGeometry), value.fields);
+
+				return new OSMEntity(OSMType.WAY, value.id, null, c, null, null);
+			}
+		});
+
+		DataSet<OSMEntity> f4 = rs.retRelations.map(new MapFunction<Relation, OSMEntity>() {
+			public OSMEntity map(Relation value) {
+
+				ArrayList<ARelated> related = new ArrayList<ARelated>();
+				// grab all the relations
+				if (value.relatedObjects != null) {
+					for (RelatedObject o : value.relatedObjects) {
+						related.add(new ARelated(o.relatedId, o.type, o.role));
+					}
+				}
+
+				ARelation r = new ARelation(value.id, value.fields, related);
+
+				return new OSMEntity(OSMType.RELATION, value.id, null, null, null, r);
+			};
+		});
+
+		AvroOutputFormat<OSMEntity> avro = new AvroOutputFormat<OSMEntity>(OSMEntity.class);
+
+		f1.union(f2).union(f3).union(f4).write(avro, outputResult);
+
+		env.execute();
+
+	}
 }
